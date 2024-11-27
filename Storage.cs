1d216ac324ffeb9a623e78d9cd89bd3dbc6c9cf5
Naresh Gupta <nareshgupta0899@gmail.com>
2024-11-27 10:06:08 +0530

    Add project files.

---
+++ Storage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Assignment4
+{
+    internal class Storage<T>
+    {
+        T value;
+
+        public void SetValue(T value)
+        {
+            this.value = value; 
+        }
+
+        public T GetValue() { return this.value; }
+    }
+}
